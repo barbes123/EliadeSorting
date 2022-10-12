5b0e2c06dbf65599973f2d02bf6f6503f9a1701a
B420 <yes.coucou@gmail.com>
2022-10-12 15:00:49 +0300

    added run_elifant/

---
+++ run/run_elifant/sorting_eliade.C
@@ -0,0 +1,47 @@
+#include "TChain.h"
+#include "TProof.h"
+#include <sstream>
+#include <string>
+#include <iostream> 
+using namespace std;
+
+string data_path= "/data/rosphere/2022_w22_dragos/root_files/";
+
+void sorting_eliade(UInt_t AddBAck = 0, int serverID=5, UInt_t first_run=195,  UInt_t last_run=195, UInt_t vol0=1, UInt_t vol1=1){
+
+ 
+ std::cout<<" sorting_eliade.C is running "<<std::endl;
+ std::cout<<" vol0 "<<vol0<<" vol1 "<<vol1<<std::endl;
+  
+ //UInt_t last_run = first_run;
+  
+  
+ for(UInt_t run=first_run;run<=last_run;++run){     
+ 	for (UInt_t vol=vol0;vol<=vol1;++vol){
+         
+        TChain *ch = new TChain("ELIADE_Tree","ELIADE_Tree");
+        string szRun, szVol;
+
+        szRun = Form("%i",run); szVol = Form("%i",vol);
+        
+        std::cout<< data_path.c_str()<<std::endl;
+
+        std::stringstream ifile;
+        
+        ifile<<Form("%srun%s_%s_ssgant1.root", data_path.c_str(), szRun.c_str(),szVol.c_str());
+        
+         if(gSystem->AccessPathName(ifile.str().c_str())){
+	        std::cout << "File "<<ifile.str().c_str()<<" does not exist, skipping" << std::endl;
+	        continue;
+    	} else {
+	        std::cout << "File "<<ifile.str().c_str()<<"  exists! " << std::endl;
+                ch->Add(Form("%sun%s_%s_ssgant1.root", data_path.c_str(), szRun.c_str(),szVol.c_str()));
+	        std::ostringstream options;
+        	options<<run<<","<<vol<<","<<AddBAck<<","<< serverID<<","<<"100";
+//     	ch->Process("/data/live/IT/dsoft/EliadeSorting/EliadeSorting.C+",options.str().c_str());
+        	ch->Process("~/EliadeSorting/DelilaSelectorEliade.C+",options.str().c_str());
+        	};
+	};   
+  };
+}
+
